d9c4d84239ce0bd0df7ef32cc1ba97afc1c3384b
Wookie3 <cbsbjb@gmail.com>
2023-11-05 23:59:18 -0500

    login setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,96 +1,50 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+// import styles from './page.module.css'
+import { useState } from 'react';
+import supabase from './utils/supabaseClient.js';
+
 
 // Login page for the app, landing page.
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+const Home = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handelLogin = async (email, password) => {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+            options: {
+                redirectTo: '/dashboard'
+            }
+        });
+        console.log(data);
+    };
+    const handelLogout = async () => {
+        const { error } = await supabase.auth.signOut();
+        console.log(error);
+    }
+    const getSession = async () => {
+        const { data, error } = await supabase.auth.getSession()
+        return ("Data: ", data, "Error: ", error)
+    }
+    return (
+        <main className='main'>
+            <div className='desc'>
+                <h1>Login Page</h1>
+                <div className='login'>
+                    <input name="email" type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input name="password"
+                        type="text"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        onKeyDown={(e) => { if (e.key == 'Enter') { handelLogin(email, password) } }}
+                    />
+                    <button onClick={() => handelLogin(email, password)}>Login</button>
+                </div>
+                <button onClick={() => handelLogout()}>Logout</button>
+            </div>
+        </main>
+    );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
