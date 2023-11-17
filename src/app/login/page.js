92c23fb6ee10aa72148baa88baca3d9c05c42ea1
Wookie3 <cbsbjb@gmail.com>
2023-11-17 17:27:53 -0500

    .

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,59 +1,9 @@
-// 'use client'
-
-// import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-// import { useRouter } from 'next/navigation'
-// import { useState } from 'react'
-
-// export default function Login() {
-//   const [email, setEmail] = useState('')
-//   const [password, setPassword] = useState('')
-//   const router = useRouter()
-//   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-//   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-//   const supabase = createClientComponentClient(supabaseUrl, supabaseKey)
-
-//   const handleSignUp = async () => {
-//     await supabase.auth.signUp({
-//       email,
-//       password,
-//       options: {
-//         emailRedirectTo: `${location.origin}/auth/callback`,
-//       },
-//     })
-//     router.refresh()
-//   }
-
-//   const handleSignIn = async () => {
-//     await supabase.auth.signInWithPassword({
-//       email,
-//       password,
-//     })
-//     router.refresh()
-//   }
-
-//   const handleSignOut = async () => {
-//     await supabase.auth.signOut()
-//     router.refresh()
-//   }
-
-//   return (
-//     <>
-//       <input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
-//       <input
-//         type="password"
-//         name="password"
-//         onChange={(e) => setPassword(e.target.value)}
-//         value={password}
-//       />
-//       <button onClick={handleSignUp}>Sign up</button>
-//       <button onClick={handleSignIn}>Sign in</button>
-//       <button onClick={handleSignOut}>Sign out</button>
-//     </>
-//   )
-// }
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 export default function Login() {
     return (
+      <div>
       <form action="/auth/login" method="post">
         <label htmlFor="email">Email</label>
         <input name="email" />
@@ -63,5 +13,19 @@ export default function Login() {
         <button formAction="/auth/sign-up">Sign Up</button>
         <button formAction="/auth/logout">Sign Out</button>
       </form>
+      </div>
     )
   }
+  
+  // const LoginForm = () => {
+  //   return (
+  //     <form action="/auth/login" method="post" className="flex w-full max-w-sm items-center space-x-2">
+  //       <Input type="email" placeholder="Email" />
+  //       <Input type="password" placeholder="Password" />
+  //       <Button type="submit">Sign In</Button>
+  //       <Button formAction="/auth/sign-up" type="submit">Sign Up</Button>
+  //       <Button  formAction="/auth/logout" type="submit" >Sign Out</Button>
+  //     </form>
+  //   )
+  // }
+  
